ef2ae2647e73b1e4341217dc125d33ddf33ac88b
guangyin xu <1809917489@qq.com>
2026-03-16 21:53:51 +0800

    Update config.py to use environment variables

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'xgy'
-    MYSQL_PASSWORD = '197111226716'
-    MYSQL_DB = 'recipe_website'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-very-strong-secret-key-in-case-env-is-not-set'
+    MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'localhost'
+    # 加上默认的用户名 xgy
+    MYSQL_USER = os.environ.get('MYSQL_USER') or 'xgy'
+    # 加上默认的密码
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or '197111226716'
+    MYSQL_DB = os.environ.get('MYSQL_DB') or 'recipe_website'
+    
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    # 阿里云百炼 API Key (建议生产环境中仍使用环境变量)
+    # 加上默认的百炼 API Key
     DASHSCOPE_API_KEY = os.environ.get('DASHSCOPE_API_KEY') or 'sk-10da423f54e44c3ea22f28bb60b1f920'
